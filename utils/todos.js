70026169364ed721b95d20d3ba8d8b53eb184428
alib4 <alibcr80547@gmail.com>
2022-08-29 02:57:37 +0430

    create this file for refactor codes

---
+++ utils/todos.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+ const path = require('path');
+
+
+ const rootDir = require('./path');
+
+ const filepath = path.join(rootDir,"/data","todos.json");
+
+ exports.getTodos = (callback) => {
+    fs.readFile(filepath,(err,fileContent) =>{
+        if (err) return callback([]);
+        callback(JSON.parse(fileContent));
+    });
+ };
+
+ exports.saveTodos = (todos,callback) => {
+    fs.writeFile(filepath,JSON.stringify(todos), (err) => {
+        callback(err);
+    });
+ };
+
+ exports.generateRandomId = () => {
+    Math.floor(Math.random*1000);
+ };
