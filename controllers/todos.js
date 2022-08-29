026c30bf8d5e4a4c574e077407dc7443217cc3a0
alib4 <alibcr80547@gmail.com>
2022-08-29 15:36:00 +0430

    develop getindex method for send data to client

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,10 +1,20 @@
 const Todo = require('../model/todo');
+const todoUtils = require('../utils/todos');
 
 exports.getIndex=(req,res) => {
-    Todo.fetchAll((todos) => {
-        res.render("index",{
-            pageTitle : "کارهای روزمره", 
-            todos,
-    });
+    todoUtils.getCompletedTodos((completedTodos) => {
+        todoUtils.getReminingTodos((reminingTodos) => {
+
+            Todo.fetchAll((todos) => {
+                res.render("index",{
+                    pageTitle : "کارهای روزمره", 
+                    todos,
+                    completedTodos,
+                    reminingTodos,
+            });
+            });
+
+        });
+
     });
 };
