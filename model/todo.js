b39381944731edafef3bb5f6cdf31ee17535fee8
alib4 <alibcr80547@gmail.com>
2022-08-30 01:08:09 +0430

    create model (table)

--- model/todo.js
+++ model/todo.js
@@ -1,46 +1,73 @@
-const todoUtils = require('../utils/todos');
-class Todo {
-    constructor(id,text,completed=false){
-        this.id=id;
-        this.text=text;
-        this.completed=completed;
-    };
+const {DataTypes} = require('sequelize');
+const sequelize = require('../utils/database');
 
-    save(callback){
-        todoUtils.getTodos((todos) => {
-            todos.push(this);
-            todoUtils.saveTodos(todos,(err) => {
-                callback(err);
-            });
-        });
-    };
+const todo = sequelize.define("Todo", {
+    //? Model Attributes
+    id: {
+        type : DataTypes.INTEGER,
+        autoIncrement : true,
+        primaryKey : true,
+        allowNull : false,
+    },
+    text : {
+        type : DataTypes.STRING,
+        allowNull : false,
+    },
+    completed : {
+        type : DataTypes.BOOLEAN,
+        defaultValue : false,
+        allowNull : true,  //? default is true
+    }
+})
 
-    static fetchAll(callback){
-        todoUtils.getTodos((todos) => {
-            callback(todos);
-        });
-   };
 
-   static deleteTodo(id,callback){
-    todoUtils.getTodos((todos) => {
-        const filteredTodos = todos.filter((t) => t.id != id );
-        todoUtils.saveTodos(filteredTodos, (err) => {
-            callback(err);
-        });
-    });
-   };
 
-   static setCompleteTodo(id,callback){
-    todoUtils.getTodos((todos) => {
-        const todoIndex = todos.findIndex((t) => t.id == id);
-        const todo = todos[todoIndex];
-        todo.completed = true;
-        todos[todoIndex] = todo;
-        todoUtils.saveTodos(todos,(err) => {
-            callback(err);
-        });
-    });
-  };
-};
 
-module.exports=Todo;
+
+
+// const todoUtils = require('../utils/todos');
+// class Todo {
+//     constructor(id,text,completed=false){
+//         this.id=id;
+//         this.text=text;
+//         this.completed=completed;
+//     };
+
+//     save(callback){
+//         todoUtils.getTodos((todos) => {
+//             todos.push(this);
+//             todoUtils.saveTodos(todos,(err) => {
+//                 callback(err);
+//             });
+//         });
+//     };
+
+//     static fetchAll(callback){
+//         todoUtils.getTodos((todos) => {
+//             callback(todos);
+//         });
+//    };
+
+//    static deleteTodo(id,callback){
+//     todoUtils.getTodos((todos) => {
+//         const filteredTodos = todos.filter((t) => t.id != id );
+//         todoUtils.saveTodos(filteredTodos, (err) => {
+//             callback(err);
+//         });
+//     });
+//    };
+
+//    static setCompleteTodo(id,callback){
+//     todoUtils.getTodos((todos) => {
+//         const todoIndex = todos.findIndex((t) => t.id == id);
+//         const todo = todos[todoIndex];
+//         todo.completed = true;
+//         todos[todoIndex] = todo;
+//         todoUtils.saveTodos(todos,(err) => {
+//             callback(err);
+//         });
+//     });
+//   };
+// };
+
+// module.exports=Todo;
