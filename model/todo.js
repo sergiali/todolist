3c4c669e5d5dced79e9f644e69b0e7a716c9a166
alib4 <alibcr80547@gmail.com>
2022-08-29 12:18:53 +0430

    refactor model codes

--- model/todo.js
+++ model/todo.js
@@ -1,8 +1,4 @@
-const fs = require('fs');
-const path = require('path');
-const rootDir = require('../utils/path');
-const filepath =path.join(rootDir,"data","todos.json");
-
+const todoUtils = require('../utils/todos');
 class Todo {
     constructor(id,text,completed=false){
         this.id=id;
@@ -11,50 +7,40 @@ class Todo {
     };
 
     save(callback){
-        fs.readFile(filepath,(err,fileContent) => {
-           // if(err) return [];
-            const todos = JSON.parse(fileContent);
+        todoUtils.getTodos((todos) => {
             todos.push(this);
-
-            fs.writeFile(filepath,JSON.stringify(todos),(err) => {
-                if(err) callback(err);
-               else return callback([]);
+            todoUtils.saveTodos(todos,(err) => {
+                callback(err);
             });
         });
     };
 
     static fetchAll(callback){
-        fs.readFile(filepath,(err,fileContent) => {
-            if(err) return [];
-            const todos = JSON.parse(fileContent);
+        todoUtils.getTodos((todos) => {
             callback(todos);
         });
    };
 
    static deleteTodo(id,callback){
-    fs.readFile(filepath,(err,fileContent) => {
-        const todos = JSON.parse(fileContent);
-        const filteredTodos = todos.filter((t) => t.id != id);
-        fs.writeFile(filepath,JSON.stringify(filteredTodos),(err) => {
-            callback(err);
-        });   
-    });
-   };
-
-   static setCompleteTodo(id,callback){
-    fs.readFile(filepath,(err,fileContent) => {
-        const todos = JSON.parse(fileContent);
-        const todoIndex = todos.findIndex((t) => t.id=id);
-
-        const todo = todos[todoIndex];
-        todo.completed = true;
-        todos[todoIndex] = todo;
-
-        fs.writeFile(filepath,JSON.stringify(todos),(err) => {
+    todoUtils.getTodos((todos) => {
+        const filteredTodos = todos.filter((t) => t.id != id );
+        todoUtils.saveTodos(filteredTodos, (err) => {
             callback(err);
         });
     });
    };
+
+   static setCompleteTodo(id,callback){
+    todoUtils.getTodos((todos) => {
+        const todoIndex = todos.findIndex((t) => t.id == id);
+        const todo = todos[todoIndex];
+        todo.completed = true;
+        todos[todoIndex] = todo;
+        todoUtils.saveTodos(todos,(err) => {
+            callback(err);
+        });
+    });
+  };
 };
 
 module.exports=Todo;
