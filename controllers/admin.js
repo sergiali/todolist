e4932c856c217f632fc9ebbaf9698244e0ff53c6
alib4 <alibcr80547@gmail.com>
2022-08-30 11:30:51 +0430

    update admin controller to crud with database

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,26 +1,26 @@
 const Todo = require('../model/todo');
-const todoUtils = require('../utils/todos');
-
 
 exports.addTodo = (req,res) => {
     if(!req.body.todo) return res.redirect("/");
-    const todo = new Todo(todoUtils.generateRandomId(),req.body.todo);
-    todo.save((err)=> {
-        if(!err) res.redirect("/");
-        else console.log(err);
-    });
+    Todo.create({text: req.body.todo}).then((result) => {
+        console.log(result);
+        res.redirect("/");
+    }).catch((err) => console.log(err));
 };
 
 exports.deleteTodo=(req,res) => {
-    Todo.deleteTodo(req.params.id,(err) => {
-        if(!err) res.redirect("/");
-        else console.log(err);    
-    });
+    Todo.destroy({where: {id: req.params.id} })
+    .then(() => res.redirect("/"))
+    .catch((err) => console.log(err));
 };
 
 exports.completedTodo = (req,res) => {
-    Todo.setCompleteTodo(req.params.id,(err) => {
-        if(!err) res.redirect("/");
-        else console.log(err);    
-    });
+    Todo.findByPk(req.params.id).then((todo) => {
+        todo.completed = true;
+        return todo.save();
+    }).then(() => res.redirect("/")).catch((err) => console.log(err));
+    // Todo.setCompleteTodo(req.params.id,(err) => {
+    //     if(!err) res.redirect("/");
+    //     else console.log(err);    
+    // });
 };
