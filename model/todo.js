d361dc171839617192b37f9fb4efab76c7e16c8f
alib4 <alibcr80547@gmail.com>
2022-08-30 20:01:52 +0430

    create schema and model with mongoose

--- model/todo.js
+++ model/todo.js
@@ -1,73 +1,47 @@
-const {DataTypes} = require('sequelize');
-const sequelize = require('../utils/database');
+const mongoose = require('mongoose');
 
-const Todo = sequelize.define("Todo", {
-    //? Model Attributes
-    id: {
-        type : DataTypes.INTEGER,
-        autoIncrement : true,
-        primaryKey : true,
-        allowNull : false,
+const todoSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3,
+        max: 255,
     },
-    text : {
-        type : DataTypes.STRING,
-        allowNull : false,
+    completed: {
+        type: Boolean,
+        required: true,
+        default: false,
     },
-    completed : {
-        type : DataTypes.BOOLEAN,
-        defaultValue : false,
-        allowNull : true,  //? default is true
-    }
-})
+});
+
+const Todo = mongoose.model("Todo",todoSchema);
+
+module.exports = Todo;
 
 
 
 
+// const {DataTypes} = require('sequelize');
+// const sequelize = require('../utils/database');
 
+// const Todo = sequelize.define("Todo", {
+//     //? Model Attributes
+//     id: {
+//         type : DataTypes.INTEGER,
+//         autoIncrement : true,
+//         primaryKey : true,
+//         allowNull : false,
+//     },
+//     text : {
+//         type : DataTypes.STRING,
+//         allowNull : false,
+//     },
+//     completed : {
+//         type : DataTypes.BOOLEAN,
+//         defaultValue : false,
+//         allowNull : true,  //? default is true
+//     }
+// })
 
-// const todoUtils = require('../utils/todos');
-// class Todo {
-//     constructor(id,text,completed=false){
-//         this.id=id;
-//         this.text=text;
-//         this.completed=completed;
-//     };
-
-//     save(callback){
-//         todoUtils.getTodos((todos) => {
-//             todos.push(this);
-//             todoUtils.saveTodos(todos,(err) => {
-//                 callback(err);
-//             });
-//         });
-//     };
-
-//     static fetchAll(callback){
-//         todoUtils.getTodos((todos) => {
-//             callback(todos);
-//         });
-//    };
-
-//    static deleteTodo(id,callback){
-//     todoUtils.getTodos((todos) => {
-//         const filteredTodos = todos.filter((t) => t.id != id );
-//         todoUtils.saveTodos(filteredTodos, (err) => {
-//             callback(err);
-//         });
-//     });
-//    };
-
-//    static setCompleteTodo(id,callback){
-//     todoUtils.getTodos((todos) => {
-//         const todoIndex = todos.findIndex((t) => t.id == id);
-//         const todo = todos[todoIndex];
-//         todo.completed = true;
-//         todos[todoIndex] = todo;
-//         todoUtils.saveTodos(todos,(err) => {
-//             callback(err);
-//         });
-//     });
-//   };
-// };
-
-module.exports=Todo;
+// module.exports=Todo;
