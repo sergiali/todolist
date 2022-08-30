17404a41d2d43be64003e0a3f19b9eda9f3bb74d
alib4 <alibcr80547@gmail.com>
2022-08-30 11:31:47 +0430

    update to show completed and remaining todos

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,20 +1,15 @@
 const Todo = require('../model/todo');
-const todoUtils = require('../utils/todos');
 
 exports.getIndex=(req,res) => {
-    todoUtils.getCompletedTodos((completedTodos) => {
-        todoUtils.getReminingTodos((reminingTodos) => {
-
-            Todo.fetchAll((todos) => {
-                res.render("index",{
-                    pageTitle : "کارهای روزمره", 
-                    todos,
-                    completedTodos,
-                    reminingTodos,
+    
+    Todo.findAll().then((todos) => {
+        Todo.count({ where : { completed : true }}).then((completedTodos) => {
+            res.render("index",{
+                pageTitle : "کارهای روزمره", 
+                todos,
+                completedTodos,
+                reminingTodos: todos.length - completedTodos,
             });
-            });
-
         });
-
     });
 };
