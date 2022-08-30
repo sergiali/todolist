95014a22a8be08d21517f2e20183180ec750c6d2
alib4 <alibcr80547@gmail.com>
2022-08-30 20:01:02 +0430

    connect to database with mongoose

--- utils/database.js
+++ utils/database.js
@@ -1,7 +1,16 @@
-const {Sequelize} = require('sequelize');
-const sequelize = new Sequelize("todo_db","root","alibabaei4",{
-    dialect : "mysql",
-    host: "localhost",
-});
+const mongoose = require('mongoose');
 
-module.exports = sequelize;
+mongoose.connect("mongodb://localhost/todo_db",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("connect to db!")).catch((err) => console.log(err));
+//?Connection , Schema && Model , Instance , Mongoose Methods
+
+
+// const {Sequelize} = require('sequelize');
+// const sequelize = new Sequelize("todo_db","root","alibabaei4",{
+//     dialect : "mysql",
+//     host: "localhost",
+// });
+
+// module.exports = sequelize;
